914369975abc622e24a18ca9792da35b93fb1baa
Bram Zijp <bramzijpcode@gmail.com>
2022-10-12 12:01:09 +0200

    chore: make component styles type more flexible

--- packages/css/src/types/componentStyles.ts
+++ packages/css/src/types/componentStyles.ts
@@ -1,5 +1,14 @@
 import type { CSSObject, Theme } from '@theme-ui/css';
 
-export type ComponentStyles<P, T extends Theme = Theme> = (
+export type StyleThemeFunction<T extends Theme = Theme> = (
+  theme: T
+) => CSSObject[] | CSSObject;
+
+export type ComponentStylesFn<P, T extends Theme = Theme> = (
   props: P
-) => (theme: T) => CSSObject[] | CSSObject;
+) => CSSObject[] | CSSObject | StyleThemeFunction<T>;
+
+export type ComponentStyles<
+  P extends unknown | undefined = undefined,
+  T extends Theme = Theme
+> = P extends undefined ? CSSObject[] | CSSObject : ComponentStylesFn<P, T>;
